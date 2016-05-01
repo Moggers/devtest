db2516f80c15b82f550bc51f7055f8a924de9574
matt kramara <mattkramara@gmail.com>
2016-05-02 00:47:18 +0800

    added migration

---
+++ devtest/database/migrations/2016_05_01_161810_create_properties_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::create('properties', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->integer('rooms');
+			$table->integer('bathrooms');
+			$table->integer('storeys');
+			$table->integer('garages');
+			$table->double('price');
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+		Schema:drop('properties');
+    }
+}
